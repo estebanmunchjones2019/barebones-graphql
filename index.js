cb4a68266c50b3ec4789aa6be9e40875af9dbbc0
Esteban Munch Jones <esteban.munch.jones@gmail.com>
2023-03-24 10:06:43 +0000

    try other package

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
+// const { createHandler } = require('graphql-http/lib/use/express')
 const schema = require('./graphql/schema');
 const root = require('./graphql/resolvers')
 
 
 
 var app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
+app.all('/graphql', graphqlHTTP({
+  schema,
   rootValue: root,
-  graphiql: true,
+  graphiql: true
 }));
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
